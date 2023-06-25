fa02490d035704de2d2bd0a12161dbf05857bd97
saifrahman007 <Sottanweshi746@gmail.com>
2023-06-25 19:34:12 +0600

    time

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
+import logo from "../../../assets/logo.png";
+import moment from 'moment';
+
 
 const Header = () => {
-    return (
-        <div>
-            <h2>This is Header</h2>
-        </div>
-    );
+  return (
+    <div>
+      <div className="text-center">
+        <img src={logo} alt="" />
+        <p className="text-secondary">
+          {" "}
+          <small>Journalism without fear and Favour</small>
+        </p>
+        <p>{moment().format("dddd, MMMM D, YYYY")}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/routes/Main.jsx
+++ src/routes/Main.jsx
@@ -2,12 +2,27 @@ import React from "react";
 import Header from "../Pages/Shared/Header/Header";
 import { Outlet } from "react-router-dom";
 import Footer from "../Pages/Shared/Footer/Footer";
+import { Col, Container, Row } from "react-bootstrap";
+import LeftNav from "../Pages/Shared/LeftNav/LeftNav";
+import RightNav from "../Pages/Shared/RightNav/RightNav";
 
 const Main = () => {
   return (
     <div>
       <Header />
-      <Outlet />
+      <Container>
+        <Row>
+          <Col lg={3}>
+            <LeftNav />
+          </Col>
+          <Col lg={6}>
+            <h2>Main content Coming Here</h2>
+          </Col>
+          <Col lg={3}>
+            <RightNav />
+          </Col>
+        </Row>
+      </Container>
       <Footer />
     </div>
   );
