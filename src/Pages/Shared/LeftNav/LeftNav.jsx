49d8b4a9f8637efc7bf23bb9aad4cd0bce4b0bfb
saifrahman007 <Sottanweshi746@gmail.com>
2023-06-26 12:03:12 +0600

    data fetch

--- src/Pages/Shared/LeftNav/LeftNav.jsx
+++ src/Pages/Shared/LeftNav/LeftNav.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
 const LeftNav = () => {
-    return (
-        <div>
-            <h2>Left Nav here</h2>
-        </div>
-    );
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/categories")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.log(error));
+  }, []);
+  return (
+    <div>
+      <h2>Left Nav here</h2>
+    </div>
+  );
 };
 
 export default LeftNav;
