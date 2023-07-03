2e6b22ca8a1fdae2967c697943c97b440876cffd
saifrahman007 <Sottanweshi746@gmail.com>
2023-07-03 11:32:20 +0600

    news-card

--- src/Pages/Shared/LeftNav/LeftNav.jsx
+++ src/Pages/Shared/LeftNav/LeftNav.jsx
@@ -14,7 +14,7 @@ const LeftNav = () => {
       <h2>All category</h2>
       <div className="ps-4">
         {categories.map((category) => (
-          <p key={category._id}>
+          <p key={category.id}>
             <Link
               to={`/category/${category.id}`}
               className="text-decoration-none text-black"
